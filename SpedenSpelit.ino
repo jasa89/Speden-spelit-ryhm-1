40b171e67210d0f8ad0c6de82a210589c20c462d
Rieskamies <NLappalainen@protonmail.com>
2024-09-25 19:20:14 +0300

    Update SpedenSpelit.ino

--- SpedenSpelit.ino
+++ SpedenSpelit.ino
@@ -6,42 +6,86 @@
 // Use these 2 volatile variables for communicating between
 // loop() function and interrupt handlers
 volatile int buttonNumber = -1;           // for buttons interrupt handler
+volatile int score = 0;
+volatile int interruptCounter = 0;
+
 volatile bool newTimerInterrupt = false;  // for timer interrupt handler
+volatile bool isRunning = false;
+
+
 
 
 void setup()
 {
-  /*
-    Initialize here all modules
-  */
+  //  Initialize all modules
+  Serial.begin(9600); //TEST
+  pinMode(12,INPUT_PULLUP); //TEST
+
+  void initButtonsAndButtonInterrupts(void);
+  void initializeDisplay(void);
+  void initializeLeds();
+
 }
 
 void loop()
 {
+  byte luettu = digitalRead(12); //TEST
+  if (luettu ==LOW) {           //TEST
+    buttonNumber++;
+    Serial.print("ButtonNumber: ");
+    Serial.println(buttonNumber);
+  }
   if(buttonNumber>=0)
   {
      // start the game if buttonNumber == 4
+      isRunning = true;
+      Serial.println("Interrupts enabled");
+      luettu = 0;             //TEST
+      buttonNumber=-1;
+      
      // check the game if 0<=buttonNumber<4
   }
 
-  if(newTimerInterrupt == true)
+  if(newTimerInterrupt)
   {
-     // new random number must be generated
-     // and corresponding let must be activated
+    newTimerInterrupt = false;
+     // new random number must be generated and corresponding led must be activated
+
+    int ledNumber = random(0,4);
+    int led = ledNumber;
+    Serial.print("LED: ");
+    Serial.println(led);
+
+    
+
   }
 }
 
 void initializeTimer(void)
 {
 	// see requirements for the function from SpedenSpelit.h
+  TCCR1A = 0;         //WGM11 ja WGM10 = 0
+  TCCR1B = 11;        //WGM12 => CTC mode ja CS11 ja CS10 => prescaler jakaa 64:lla 16000 000 Hz kellon => 250 000 sykäystä/s
+  OCR1A = 250000;      // => 1000 keskeytystä/s
+  TIMSK1 = 2;         // OCIE1A = Output Compare Interrupt Enable 1A
+
 }
+
+
 ISR(TIMER1_COMPA_vect)
 {
-  /*
-  Communicate to loop() that it's time to make new random number.
-  Increase timer interrupt rate after 10 interrupts.
-  */
-  
+  //  Communicate to loop() that it's time to make new random number. Increase timer interrupt rate after 10 interrupts.
+  Serial.println("interrupt happened");   //TEST
+
+  interruptCounter++;
+
+  if (interruptCounter >= 10) {
+    OCR1A = OCR1A / 2;
+    interruptCounter = 0;
+  }
+
+  newTimerInterrupt = true;
+  Serial.println(interruptCounter);
 }
 
 
@@ -59,5 +103,10 @@ void initializeGame()
 void startTheGame()
 {
    // see requirements for the function from SpedenSpelit.h
+  initializeTimer();
+  InitializeGame();
+
+
+  interrupts();
 }
 
