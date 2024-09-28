e210b6971b2cb4d3bf5a3ac0cbe614d670827617
jpjuoppe <jari-pekka.juopperi@outlook.com>
2024-09-28 14:45:17 +0300

    Update buttons.cpp

--- buttons.cpp
+++ buttons.cpp
@@ -6,12 +6,27 @@
 void initButtonsAndButtonInterrupts(void)
 {
   // See requirements for this function from buttons.h
+PCICR |= B00000100;
+PCMSK2 |= B11110000;
+ for (int i = 0; i < 4 ; i++)
+      {
+        pinMode(PINS[i], INPUT_PULLUP);
+
+      } 
 }
 
 ISR(PCINT2_vect) {
-   /*
-     Here you implement logic for handling
-	 interrupts from 2,3,4,5 pins for Game push buttons
-	 and for pin 6 for start Game push button.
-   */
+  	static unsigned long lastInterrupTimeStamp=0;
+	unsigned long interrupTimeStamp=millis();
+        	if (interrupTimeStamp - lastInterrupTimeStamp > 50)
+        		{
+          		for (int j=0; j<4; j++)
+            		{
+              			if (digitalRead(PINS[j] == LOW))
+              			{
+                			pressedButton=PINS[j];
+              			}
+            		}
+              		lastInterrupTimeStamp=interrupTimeStamp;
+        		}
 }
