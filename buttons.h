661fb221e21489a56e966ce9191529ded8c5287f
jpjuoppe <jari-pekka.juopperi@outlook.com>
2024-10-01 17:22:45 +0300

    Update buttons.h
    Viimeisimmät muutokset

--- buttons.h
+++ buttons.h
@@ -5,17 +5,23 @@
 #include <avr/interrupt.h>
 
 const int PINS[] = {4,5,6,7};
-const byte firstPin = 4; // First PinChangeInterrupt on D-bus
-const byte lastPin =  7; // Last PinChangeInterrupt on D-bus
-volatile int pressedButton = 4;
+int changed=0;
+volatile int buttonState = 0; 
+/*  if button 1 pressed , value is 1
+    if button 2 pressed , value is 2
+    if button 3 pressed , value is 4
+    if button 4 pressed , value is 8
+    if value >=9 , two buttons pressed (for reset)
+*/ 
+
 
 /* 
   initButtonsAndButtonInterrupts subroutine is called from Setup() function
   during the initialization of Speden Spelit. This function does the following:
-  1) Initializes 4 button pins for the game = Arduino pins 2,3,4,5
-  2) Initializes 1 button pin for starting the game = Aruino pin 6
+  1) Initializes 4 button pins for the game = Arduino pins 4,5,6,7
+  2) Initializes 1 button pin for starting the game = Aruino pin 6 - not implemented for us
   3) Enables PinChangeInterrupt on D-bus in a way that interrupt
-     is generated whenever some of pins 2,3,4,5,6 is connected to LOW state
+     is generated whenever some of pins 4,5,6,7 is connected to LOW state
 
 */
 void initButtonsAndButtonInterrupts(void);
