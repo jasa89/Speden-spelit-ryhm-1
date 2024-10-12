334da6b369a452b16d6f44ab1026fb3fc5dd2ebe
jpjuoppe <jari-pekka.juopperi@outlook.com>
2024-10-12 13:10:14 +0300

    Update buttons.h
    added comments

--- buttons.h
+++ buttons.h
@@ -11,7 +11,7 @@ const int PINS[] = {4,5,6,7};
     if button 2 pressed , value is 2
     if button 3 pressed , value is 4
     if button 4 pressed , value is 8
-    if value >=9 , two buttons pressed (for reset)
+    if any other value, two buttons pressed (for reset)
 */ 
 
 
@@ -19,8 +19,7 @@ const int PINS[] = {4,5,6,7};
   initButtonsAndButtonInterrupts subroutine is called from Setup() function
   during the initialization of Speden Spelit. This function does the following:
   1) Initializes 4 button pins for the game = Arduino pins 4,5,6,7
-  2) Initializes 1 button pin for starting the game = Aruino pin 6 - not implemented for us
-  3) Enables PinChangeInterrupt on D-bus in a way that interrupt
+  2) Enables PinChangeInterrupt on D-bus in a way that interrupt
      is generated whenever some of pins 4,5,6,7 is connected to LOW state
 
 */
@@ -30,6 +29,6 @@ void initButtonsAndButtonInterrupts(void);
 // Intoduce PCINT2_vect Interrupt SeRvice (ISR) function for Pin Change Interrupt.
 ISR(PCINT2_vect); 
 //functons interrupt uses
-int readButton(void);
-int whatButton(int);
+int readButton(void); //read button and reurn its value
+int whatButton(int);  // translates button to corresponding led
 #endif;
